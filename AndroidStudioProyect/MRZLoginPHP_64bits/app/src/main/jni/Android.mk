b0c98622e088065ef4ce06c67d07c0c3c0520010
MrEoZ <41388002+MrEoZ-MRZ@users.noreply.github.com>
2020-03-03 13:14:57 -0300

    Add files via upload

--- AndroidStudioProyect/MRZLoginPHP_64bits/app/src/main/jni/Android.mk
+++ AndroidStudioProyect/MRZLoginPHP_64bits/app/src/main/jni/Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := MRZ
 
-LOCAL_SRC_FILES := Main.cpp \ src/KittyMemory/KittyMemory.cpp \ src/KittyMemory/MemoryPatch.cpp \
+LOCAL_SRC_FILES := Main.cpp \ src/KittyMemory/KittyMemory.cpp \ src/KittyMemory/MemoryPatch.cpp \ Unity/Vector2.hpp \ Unity/Vector3.hpp \ Unity/Quaternion.hpp \ Substrate/hde64.c \ Substrate/SubstrateDebug.cpp \ Substrate/SubstrateHook.cpp \ Substrate/SubstratePosixMemory.cpp \
 				   
 include $(BUILD_SHARED_LIBRARY)
